d7c6e9b1089eb1a578c68276e4b3cb09dabf2e66
enes k <eneskaraoglu@gmail.com>
2023-05-07 22:23:10 +0300

    Update insert.sql

--- src/main/resources/insert.sql
+++ src/main/resources/insert.sql
@@ -9,25 +9,25 @@ INSERT INTO t_katalog (katalog_adi,katalog_kodu) VALUES
 	 ('Katalog 3','K3');
 	 
 INSERT INTO t_envanter (envanter_adi,envanter_kodu,envanter_miktar,kritik_miktar,katalog_id) VALUES
-	 ('H001','Somun',0,1000,1),
-	 ('H002','Vida',0,1000,1),
-	 ('Y002','V2 Yarı Mamü',0,200,2),
-	 ('Y001','AZ2 Yarı Mamül',0,100,2),	 
-	 ('SON1','Kalem',0,100,3),
-	 ('SON2','Bike',0,75,3),
-	 ('SON3','Mouse',0,50,3);
+	 ('H001','Somun',100,1000,1),
+	 ('H002','Vida',400,1000,1),
+	 ('Y002','V2 Yarı Mamü',750,200,2),
+	 ('Y001','AZ2 Yarı Mamül',500,100,2),	 
+	 ('SON1','Kalem',125,100,3),
+	 ('SON2','Bike',900,75,3),
+	 ('SON3','Mouse',450,50,3);
 
 INSERT INTO t_depo_envanter (depo_id,envanter_id,depo_miktar) VALUES
-	 (1,1,0),
+	 (1,1,100),
 	 (1,2,0),
-	 (2,1,0),
-	 (2,2,0),
-	 (2,3,0),
-	 (3,2,0),
-	 (4,3,0),
-	 (4,2,0),
-	 (5,2,0),
-	 (6,3,0),
-	 (6,2,0),
-	 (7,3,0),
-	 (7,2,0);
+	 (2,1,200),
+	 (2,2,150),
+	 (2,3,50),
+	 (3,2,750),
+	 (4,3,150),
+	 (4,2,350),
+	 (5,2,125),
+	 (6,3,750),
+	 (6,2,150),
+	 (7,3,100),
+	 (7,2,350);
